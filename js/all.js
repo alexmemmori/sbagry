b606f11501e474bae4fedeaae264b6aa1e2ba183
Alexey Ivanov <alexmemmori@yandex.ru>
2018-03-30 22:35:25 +0300

    Новые страницы + правки
    Добавил несколько новых страниц + сделал куки + всё перепроверил, поправил косяки. В этих страницах не будет меняться ничего. Александр просил сделать коммит, оставшаяся часть страниц будет завтра в примерно это же время.

--- js/all.js
+++ js/all.js
@@ -45,12 +45,52 @@ $('.categories-section .container').slick({
 $('.user-login-panel .photo').on('click', function(event) {
   event.preventDefault();
   $('.user-login-panel').toggleClass('show');
-})
+});
+
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires*1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) { 
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for(var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];    
+    if (propValue !== true) { 
+      updatedCookie += "=" + propValue;
+     }
+  }
+
+  document.cookie = updatedCookie;
+};
+
+// возвращает cookie с именем name, если есть, если нет, то undefined
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+};
 
 // Кнопка "Закрыть" на нижней панели "Смотреть презентацию"
+  if(Cookies.get('close-view') == "true") {
+    $('.view-modal-container').addClass('hidden');
+  }
 
 $('.close-view-modal-btn').on('click', function(event) {
   event.preventDefault();
   $(this).parent().addClass('hidden');
-  // Советую дополнительно привязать скрытие кнопки по кукам
-})
+  Cookies.set('close-view', "true", { expires: 7, path: '/' });
+});
